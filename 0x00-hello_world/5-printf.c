873772ad30d9da7467911e5899dd6961bd46a31a
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-03-16 03:34:27 -0700

    ja

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - program to print statement using printf statement
+* Return variable result for main
+* return 0;(success)
+*/
+int main(void)
+{
+char alxpro[] = "with proper grammar, but the outcome is a piece of art,";
+
+printf("%s\n", alxpro);
+
+return (0);
+}
