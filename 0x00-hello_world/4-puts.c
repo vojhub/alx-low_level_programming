e79dcbb5ef86f39cb369a538b09402e10a92e6bd
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-03-16 02:44:34 -0700

    has

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-*Entry point
+*Script that print out using puts function
+*Should not use printf()
+*Return 0; (success)
 */
-
 int main(void)
 {
-    char alxpro [] = "\"Programming is like building a multilingual puzzle";
-    puts (alxpro);
-
+    char alxpro[]="\"Programming is like building a multilingual puzzle";
+    puts(alxpro);
     return 0;
 }
