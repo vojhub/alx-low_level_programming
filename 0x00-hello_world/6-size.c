eb36a21565b6cdc932803021ac1b421a5f28e520
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-03-16 03:58:07 -0700

    is

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
+printf("Size of char: %zu byte\n", sizeof(char));
 printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of long: %zu bytes\n", sizeof(long));
-printf("Size of float: %zu bytes\n", sizeof(float));
 printf("Size of double: %zu bytes\n", sizeof(double));
-printf("Size of char: %zu byte\n", sizeof(char));
+printf("Size of float: %zu bytes\n", sizeof(float));
 return (0);
 }
