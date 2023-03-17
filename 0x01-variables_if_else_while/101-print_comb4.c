264793efdacceef6d3883d44dd2d3fd9a921c641
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-03-17 08:41:31 -0700

    s

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,29 +7,28 @@
  */
 int main(void)
 {
-	int x, y, z;
+int n, m, l;
 
-
-	for (x = 0; x < 10; x++)
-	{
-	    for (z = 0; z < 10; z++){
-		for (y = 0; y < 10; y++)
-		{
-			putchar((x % 10) + '0');
-			putchar((y % 10) + '0');
-            putchar((z % 10) + '0');
-
-			if (x == 9 && y == 9)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+for (n = 48; n < 58; n++)
+{
+for (m = 49; m < 58; m++)
+{
+for (l = 50; l < 58; l++)
+{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
-	    }
 	}
-
-	putchar('\n');
-
-	return (0);
+putchar('\n');
+return (0);
 }
-
