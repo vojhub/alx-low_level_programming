772d79bb5c9d3c8ca6a998c48d2940d2b16510cb
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-03-17 03:01:46 -0700

    yes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,14 @@
 *
 * Return: Always 1 (success)
 */
-int main(void) {
+int main(void)
+{
 const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-const ssize_t len = sizeof(msg) - 1;  /*  exclude null terminator */
+
+const ssize_t len = sizeof(msg) - 1; 
+/*  exclude null terminator */
 
 const ssize_t written = write(STDERR_FILENO, msg, len);
-return (written == len) ? 1 : 0;
+return (written == len) ? 1 : (0);
 }
 
---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Check if number is positive or negative or zero
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int a = 0;
+
+if (a > 0)
+{
+printf("is positive\n");
+}
+else if (a == 0)
+{
+printf("is zero\n");
+}
+else
+{
+printf("is negative\n");
+}
+return (0);
+}
