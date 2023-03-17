72ce1ee562dc9ec129d1e4d5951c7190f0765ac7
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-03-17 08:27:39 -0700

    s

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        a comma followed by a space, in ascending order.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x, y, z;
+
+
+	for (x = 0; x < 10; x++)
+	{
+	    for (z = 0; z < 10; z++){
+		for (y = 0; y < 10; y++)
+		{
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+            putchar((z % 10) + '0');
+
+			if (x == 9 && y == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	    }
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
