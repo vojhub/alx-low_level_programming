59d0b8a278b639313d7e06c45d62f1eac989b61f
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-03-17 04:12:39 -0700

    sa

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,20 +13,21 @@
 int main(void)
 {
 int n;
-int last_digit = n % 10;
+int a;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (last_digit > 5)
+a = n % 10;
+if (a > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, a);
 }
-else if (last_digit == 0)
+else if (a == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, a);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 }
 return (0);
 }
