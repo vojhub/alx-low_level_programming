9337b9d34517eefa2cd3f46e49e4db49461b88fd
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-03-21 03:45:40 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+*Main -entry point
+*
+*print putchar command
+*
+*return 0 (success)
+*/
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
