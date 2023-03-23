9ef63e9efc684616040d1de4a4e16f33715a20ac
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-03-23 02:28:54 -0700

    scv

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,20 +8,21 @@
  */
 void print_square(int size)
 {
-int a, b;
+	int a, b;
 
-if (size > 0)
-{
-for (a = 0 ; a < size ; a++)
-{
-for (b = 0 ; b < size ; b++)
-{
-_putchar(35);
-}
-_putchar('\n');
-}
-}
-else
-{
-_putchar('\n');
+	if (size > 0)
+	{
+		for (a = 0 ; a < size ; a++)
+		{
+			for (b = 0 ; b < size ; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
