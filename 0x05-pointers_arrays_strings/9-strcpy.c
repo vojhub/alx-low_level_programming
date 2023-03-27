019e78aea469282abc20865f0320db0cb9dea995
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-03-27 10:59:36 -0700

    na

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,16 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int i;
 
-	while (src[index])
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
 	}
+
+	dest[i++] = '\0';
 
 	return (dest);
 }
