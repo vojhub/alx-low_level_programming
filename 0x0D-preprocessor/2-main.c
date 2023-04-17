dc2d3a8b71dcd04a3ef9079ee067e06346417692
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-04-17 02:38:40 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Entry point of the program
+ *__file__ prints name of currnt file processed
+ *Return 0 if successful
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
