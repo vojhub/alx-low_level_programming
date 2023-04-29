4e78f594b1c7ceea52ea0d2d82bc4848d38f6555
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-04-29 07:58:45 -0700

    tasi

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 
 /**
- *print_listint - Prints all of the linked list.
- *@h: list-t list pointer of the head.
- *return: nodes number in list-t list.
+*print_listint - prints all the elements of a linked list
+*@head: pointer to the head of a linked list
+*type listint_t and returns the number 
+*return: nodes number in list-t list.
 */
-
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *head)
 {
-size_t num = 0;
-while (h)
+size_t num_nodes = 0;
+while (head)
 {
-printf("%d\n", h->n);
-num++;
-h = h->next;
+printf("%d\n", head->n);
+num_nodes++;
+head = head->next;
 }
-
-return (num);
+return (num_nodes);
 }
-
