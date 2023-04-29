f6b19437a36602034809ce4cb8a9b4b1e4c03fd9
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-04-29 02:17:06 -0700

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,12 +6,12 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t nodes = 0;
+size_t num = 0;
 while (h)
-{
-nodes++;
-printf("%d\n", h->n);
+{printf("%d\n", h->n);
+num++;
 h = h->next;
 }
-return (nodes);
+return (num);
 }
+
