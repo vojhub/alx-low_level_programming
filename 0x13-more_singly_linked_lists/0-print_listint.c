de81e5f85272d97ddb3300285e8f7f38a7e0bba8
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-04-29 02:51:04 -0700

    last

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,21 @@
 #include "lists.h"
+
 /**
  *print_listint - Prints all of the linked list.
  *@h: list-t list pointer of the head.
  *return: nodes number in list-t list.
 */
-size_t listint_len(const listint_t *h);
-{
-size_t nodes = 0;
 
+size_t print_listint(const listint_t *h)
+{
+size_t num = 0;
 while (h)
 {
-	nodes++;
-	printf("%d\n", h->n);
-	h = h->next;
+printf("%d\n", h->n);
+num++;
+h = h->next;
 }
-return (nodes);
+
+return (num);
 }
+
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,13 +1,17 @@
 #include "lists.h"
+
 /**
  *free_listint2 - Frees a listint_t list.
  *@head: A pointer to listint_t list.
  */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	if (head == NULL)
-	return;
+	
+if (head == NULL)
+return;
+
 while (*head)
 {
 	temp = (*head)->next;
