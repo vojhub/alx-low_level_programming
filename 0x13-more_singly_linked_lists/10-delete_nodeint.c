86860814faafb813e4551730979e324a754de609
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-04-30 23:59:46 -0700

    ja

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,31 +5,32 @@
 *@index: index of the node to delete
 *Return: 1 (Success), or -1 (Fail)
 */
-int delete_fapint_at_index(listint_t **head, unsigned int index)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *tmp, *copy = *head;
-unsigned int fap;
+listint_t *temp = *head;
+listint_t *current = NULL;
+unsigned int i = 0;
 
-if (copy == NULL)
+if (*head == NULL)
 return (-1);
 
 if (index == 0)
 {
-		*head = (*head)->next;
-		free(copy);
-		return (1);
-}
-
-for (fap = 0; fap < (index - 1); fap++)
-{
-if (copy->next == NULL)
-return (-1);
-
-copy = copy->next;
-}
-
-tmp = copy->next;
-copy->next = tmp->next;
-free(tmp);
+*head = (*head)->next;
+free(temp);
 return (1);
 }
+while (i < index - 1)
+{
+if (!temp || !(temp->next))
+return (-1);
+temp = temp->next;
+i++;
+}
+current = temp->next;
+temp->next = current->next;
+free(current);
+
+return (1);
+}
+
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,32 +6,33 @@
  *@n: The integer for the new node to contain
  * Return: pointer to the new node, or NULL
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *temp = *head;
-listint_t *current = NULL;
-unsigned int p = 0;
+listint_t *new, *copy = *head;
+unsigned int node;
 
-if (*head == NULL)
-return (-1);
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+		return (NULL);
 
-if (index == 0)
+new->n = n;
+
+if (idx == 0)
 {
-*head = (*head)->next;
-free(temp);
-return (1);
-}
-while (p < index - 1)
-{
-if (!temp || !(temp->next))
-return (-1);
-temp = temp->next;
-i++;
-}
-current = temp->next;
-temp->next = current->next;
-free(current);
-
-return (1);
+new->next = copy;
+*head = new;
+return (new);
 }
 
+for (node = 0; node < (idx - 1); node++)
+{
+if (copy == NULL || copy->next == NULL)
+return (NULL);
+
+copy = copy->next;
+}
+
+new->next = copy->next;
+copy->next = new;
+return (new);
+}
