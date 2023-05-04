47749133f0a3a847193879736d5bbbed2ed4eecd
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-05-04 03:46:02 -0700

    has

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,11 +10,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= sizeof(unsigned long int) * 8)
-{
+if (index >= (sizeof(unsigned long int) * 8))
 return (-1);
-}
-unsigned long int mask = ~(1UL << index);
-*n &= mask;
+
+*n &= ~(1 << index);
+
 return (1);
 }
