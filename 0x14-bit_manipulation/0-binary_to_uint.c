49ec768fd986c4dfa86c32a475ec9e13ee376528
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-05-04 03:04:57 -0700

    kxc

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int n;
+unsigned int valu = 0;
+
+if (!b)
+return (0);
+for (n = 0; b[n]; n++)
+{
+if (b[n] < '0' || b[n] > '1')
+return (0);
+valu = 2 * valu + (b[n] - '0');
+}
+return (valu);
+}
