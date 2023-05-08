e8a4396110ba182401ea6ae280a89eb9c27814f1
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-05-08 03:49:09 -0700

    hax

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <main.h>
+/**
+*create_file -  create file text file print to STDOUT.
+*@filename: text file being created
+*@text_content : pointer number of letters to be read
+* return -1 if fails return 1 success
+*/
+int create_file(const char *filename, char *text_content)
+{
+if (filename == NULL)
+{
+return (-1);
+}
+int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+if (bytes_written == -1)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
+
