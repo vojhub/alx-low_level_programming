88fccb59395c4956636f1027e6fecd8ef2a95971
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-05-08 03:59:52 -0700

    js

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,27 +7,17 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int create_file(const char *filename, char *text_content)
-{
-if (filename == NULL)
-{
-return (-1);
-}
-int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+char *buf;
+ssize_t fd;
+ssize_t w;
+ssize_t t;
+fd = open(filename, O_RDONLY);
 if (fd == -1)
-{
-return (-1);
-}
-if (text_content != NULL)
-{
-ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-if (bytes_written == -1)
-{
+return (0);
+buf = malloc(sizeof(char) * letters);
+t = read(fd, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
+free(buf);
 close(fd);
-return (-1);
-}
-}
-close(fd);
-return (1);
-}
+return (w);
 }
