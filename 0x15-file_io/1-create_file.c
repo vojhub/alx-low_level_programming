e224b7fca63051295d96b6259f859cf2a67f9eb7
Geoffrey Karanja <swiftsagewriter@gmail.com>
2023-05-08 06:12:24 -0700

    svf

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,24 +8,20 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-if (filename == NULL)
-{
+int fd;
+int nletters;
+int nhs;
+if (!filename)
 return (-1);
-}
-int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1)
-{
 return (-1);
-}
-if (text_content != NULL)
-{
-ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-if (bytes_written == -1)
-{
-close(fd);
+if (!text_content)
+text_content = "";
+for (nletters = 0; text_content[nletters]; nletters++);
+nhs = write(fd, text_content, nletters);
+if (nhs == -1)
 return (-1);
-}
-}
 close(fd);
 return (1);
 }
